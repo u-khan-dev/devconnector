ce60aca4b17cb5088d410b8294020dd05816ae9c
Umar Khan <knarb137@gmail.com>
2021-08-21 17:34:03 -0400

    remove unused className

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -16,7 +16,7 @@ const Landing = ({ isAuthenticated }) => {
             Create a developer profile/portfolio, share posts and get help from
             other developers
           </p>
-          <div className="buttons">
+          <div>
             <Link to="/register" className="btn btn-primary">
               Sign Up
             </Link>
